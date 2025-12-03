1e0c0d712c8d15248a0dd4453df8533082ed50b5
Adelina <asg40401@educastur.es>
2025-12-03 17:15:09 +0100

    Añadida dependencia Gson

--- src/main/java/edu/examen/ProyectoExamen_Adelina/App.java
+++ src/main/java/edu/examen/ProyectoExamen_Adelina/App.java
@@ -6,5 +6,6 @@ package edu.examen.ProyectoExamen_Adelina;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("");
     }
 }
